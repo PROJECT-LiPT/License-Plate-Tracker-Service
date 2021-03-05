397c39fe7017ff2ebe7cd61784fc0a5eee16ef31
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-03-05 19:53:39 +0700

    Delete ui directory

--- ui/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-//url depends on python flask app host server.
-const url = 'http://127.0.0.1:5000/prediction/';
-
-//api for fetching asynch prediction endpoints from backend
-// export const fetchLp = () => axios.post(url);
-
-//api for uploading images to server
-export const uploadLp = (newLp) => axios.post(url, newLp);
--- ui/src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import licensePlate from './licensePlate';
-
-export const reducers = combineReducers({ licensePlate });
